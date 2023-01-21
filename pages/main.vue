f2abcb44a5910f71d2337ea3ad141d47d9e16268
magnae2016 <magnae2016@gmail.com>
2023-01-21 16:58:42 +0900

    Update displaying received data

--- pages/main.vue
+++ pages/main.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
     <p>메인 페이지입니다.</p>
+    <div>
+      {{ products }}
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 export default {
+  data() {
+    return {
+      products: [],
+    }
+  },
   async created() {
     const response = await axios.get('http://localhost:3000/products')
-    console.log(response)
+    this.products = response.data
   },
 }
 </script>
