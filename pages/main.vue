634cfb06be50a0c8e7a5d14f94b27f811691d02f
magnae2016 <magnae2016@gmail.com>
2023-01-21 17:14:00 +0900

    Apply asyncData

--- pages/main.vue
+++ pages/main.vue
@@ -10,15 +10,12 @@
 <script>
 import axios from 'axios'
 export default {
-  data() {
-    return {
-      products: [],
-    }
-  },
-  async created() {
+  async asyncData() {
     const response = await axios.get('http://localhost:3000/products')
-    this.products = response.data
+    const products = response.data
+    return { products }
   },
+  created() {},
 }
 </script>
 
