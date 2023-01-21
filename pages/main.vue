fcad0821dc0104c6e5ae306a1b37ca58b821b4ee
magnae2016 <magnae2016@gmail.com>
2023-01-21 17:32:25 +0900

    Update product list display function

--- pages/main.vue
+++ pages/main.vue
@@ -2,7 +2,14 @@
   <div>
     <p>메인 페이지입니다.</p>
     <div>
-      {{ products }}
+      <ul>
+        <li v-for="product in products" :key="product.id">
+          {{ product }}
+          <img :src="product.imageUrl" :alt="product.name" />
+          <p>{{ product.name }}</p>
+          <p>{{ product.price }}</p>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -12,10 +19,12 @@ import axios from 'axios'
 export default {
   async asyncData() {
     const response = await axios.get('http://localhost:3000/products')
-    const products = response.data
+    const products = response.data.map((item) => ({
+      ...item,
+      imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+    }))
     return { products }
   },
-  created() {},
 }
 </script>
 
