4c8482b361dc1ce625547fddcd0ee3b71aa1bdea
magnae2016 <magnae2016@gmail.com>
2023-01-21 22:43:15 +0900

    Update product detail page layout

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -1,16 +1,52 @@
 <template>
   <div>
-    <h1>상세 페이지</h1>
+    <div class="container">
+      <div class="main-panel">
+        <img
+          class="product-image"
+          :src="product.imageUrl"
+          :alt="product.name"
+        />
+      </div>
+      <div class="side-panel">
+        <p class="name">{{ product.name }}</p>
+        <p class="price">{{ product.price }}</p>
+        <!-- <button type="button" @click="addToCart">Add to Cart</button> -->
+      </div>
+    </div>
   </div>
 </template>
   
-  <script>
+<script>
+import { fetchProductById } from '@/api/index'
 export default {
+  async asyncData({ params }) {
+    const response = await fetchProductById(params.id)
+    const product = response.data
+    return { product }
+  },
   created() {
-    console.log(this.$route)
+    console.log(this.$route.params.id)
   },
 }
 </script>
   
-  <style>
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0;
+}
+.product-image {
+  width: 500px;
+  height: 375px;
+}
+.side-panel {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 220px;
+  text-align: center;
+  padding: 0 1rem;
+}
 </style>
