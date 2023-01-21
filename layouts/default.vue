3d0cc076247cef24342a4f8d9085d646872078bd
magnae2016 <magnae2016@gmail.com>
2023-01-21 17:39:29 +0900

    Update Routing and Styling Cleanup

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <header>
+      <h1>{{ $route.name }} 페이지</h1>
       <nuxtLink to="/">홈페이지</nuxtLink>
       <nuxtLink to="/main">메인 홈페이지</nuxtLink>
       <nuxtLink to="/product">상품 홈페이지</nuxtLink>
-      <h1>{{ $route.name }} 페이지</h1>
     </header>
     <Nuxt />
   </div>
