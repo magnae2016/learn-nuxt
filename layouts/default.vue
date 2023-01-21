a7a9033775fb3f12d1088c7e6b236c5a4d93b4a5
magnae2016 <magnae2016@gmail.com>
2023-01-21 16:25:24 +0900

    Add nuxt-link

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <h1>{{ $route.name }} 페이지</h1>
+    <header>
+      <nuxtLink to="/">홈페이지</nuxtLink>
+      <nuxtLink to="/main">메인 홈페이지</nuxtLink>
+      <nuxtLink to="/product">상품 홈페이지</nuxtLink>
+      <h1>{{ $route.name }} 페이지</h1>
+    </header>
     <Nuxt />
   </div>
 </template>
